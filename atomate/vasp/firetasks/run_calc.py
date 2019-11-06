946688d3074e7e5768f233802b482e0b69eb465a
Ryan Kingsbury <RKingsbury@lbl.gov>
2019-11-06 12:54:16 -0800

    Add variations on SCAN workflow

--- atomate/vasp/firetasks/run_calc.py
+++ atomate/vasp/firetasks/run_calc.py
@@ -137,6 +137,14 @@ class RunVaspCustodian(FiretaskBase):
             jobs = VaspJob.metagga_opt_run(vasp_cmd, auto_npar=auto_npar,
                                                  ediffg=self.get("ediffg"),
                                                  half_kpts_first_relax=self.get("half_kpts_first_relax", HALF_KPOINTS_FIRST_RELAX))
+        
+        elif job_type == "metagga_full_opt":
+            jobs = VaspJob.metagga_full_opt(vasp_cmd, auto_npar=auto_npar,
+                                                 ediffg=self.get("ediffg"),
+                                                 half_kpts_first_relax=self.get("half_kpts_first_relax", HALF_KPOINTS_FIRST_RELAX))
+        
+        elif job_type == "metagga_static":
+            jobs = VaspJob.metagga_static(vasp_cmd, auto_npar=auto_npar))
 
         elif job_type == "full_opt_run":
             jobs = VaspJob.full_opt_run(vasp_cmd, auto_npar=auto_npar,
