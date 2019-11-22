7ee81f0ca038bfd319f478e2096a58203cda4361
Ryan Kingsbury <RKingsbury@lbl.gov>
2019-11-22 10:07:26 -0800

    fix for vdw kernel

--- atomate/vasp/fireworks/core.py
+++ atomate/vasp/fireworks/core.py
@@ -72,7 +72,7 @@ class OptimizeFW(Firework):
         
         if vasp_input_set.vdw is not None:
             # Copy the pre-compiled VdW kernel for VASP, if required
-            t.CopyFiles("$VDW_KERNAL_BINDAT")
+            t.append(CopyFiles("$VDW_KERNAL_BINDAT"))
 
         t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, job_type=job_type,
                                   max_force_threshold=max_force_threshold,
@@ -81,7 +81,7 @@ class OptimizeFW(Firework):
                                   half_kpts_first_relax=half_kpts_first_relax))
         
         # Delete the VdW kernel
-        t.DeleteFiles(["vdw_kernel.bindat"])
+        t.append(DeleteFiles(["vdw_kernel.bindat"]))
 
         t.append(PassCalcLocs(name=name))
         t.append(
@@ -130,7 +130,7 @@ class StaticFW(Firework):
 
         if vasp_input_set.vdw is not None:
             # Copy the pre-compiled VdW kernel for VASP, if required
-            t.CopyFiles("$VDW_KERNAL_BINDAT")
+            t.append(CopyFiles("$VDW_KERNAL_BINDAT"))
 
         if prev_calc_dir:
             t.append(CopyVaspOutputs(calc_dir=prev_calc_dir, contcar_to_poscar=True))
@@ -151,8 +151,8 @@ class StaticFW(Firework):
         t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, auto_npar=">>auto_npar<<"))
 
         # Delete the VdW kernel
-        t.DeleteFiles(["vdw_kernel.bindat"])
-        
+        t.append(DeleteFiles(["vdw_kernel.bindat"]))
+
         t.append(PassCalcLocs(name=name))
         t.append(
             VaspToDb(db_file=db_file, **vasptodb_kwargs))
